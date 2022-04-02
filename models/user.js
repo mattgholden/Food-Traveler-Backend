c6d4d4edb012b967f8ded3a121efdcca7d04980a
mgholden <mgholden27@gmail.com>
2022-04-01 22:15:08 -0400

    Remove confirm password

--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ const mongoose = require ('../db/connection');
 const userSchema = new mongoose.Schema({
     username: {type: String, unique: true, required: true},
     password: {type: String, required: true},
-    confirmPassword: {type: String, required: true},
     //dietaryRestrictions
 })
 
